b9b0d0c75cbb3d265fb4f8675bf2ce15e2daee99
pucciorenzo <vincenzo.cosi96@gmail.com>
2024-03-19 19:39:17 +0100

    Added auto-pairs

---
+++ lua/plugins/auto-pairs.lua
@@ -0,0 +1,21 @@
+return {
+	"windwp/nvim-autopairs",
+	config = function()
+		local npairs = require("nvim-autopairs")
+		npairs.setup({
+			fast_wrap = {
+				map = "<M-e>",
+				chars = { "{", "[", "(", '"', "'" },
+				pattern = [=[[%'%"%>%]%)%}%,]]=],
+				end_key = "$",
+				before_key = "h",
+				after_key = "l",
+				cursor_pos_before = true,
+				keys = "qwertyuiopzxcvbnmasdfghjkl",
+				manual_position = true,
+				highlight = "Search",
+				highlight_grey = "Comment",
+			},
+		})
+	end,
+}
