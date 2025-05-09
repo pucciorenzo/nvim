7a44e0fba770ccaa7c2c7acdb646dd22f3f756c4
Vincenzo Cosi <vincenzocosi@Mac-mini-di-Vincenzo.local>
2025-05-09 23:14:27 +0200

    telescope for mac

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,24 @@ return {
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      require("telescope").setup({
+        defaults = {
+          prompt_prefix = "🔍 ",
+          selection_caret = " ",
+          path_display = { "smart" },
+          file_ignore_patterns = { ".git/", "node_modules", "%.lock", "%.class", "%.o" },
+        },
+        pickers = {
+          find_files = {
+            find_command = { "fd", "--type", "f", "--hidden", "--exclude", ".git" },
+          },
+          live_grep = {
+            additional_args = function()
+              return { "--hidden", "--glob", "!.git/*" }
+            end,
+          },
+        },
+      })
     end,
   },
   {
@@ -20,3 +38,4 @@ return {
     end,
   },
 }
+
